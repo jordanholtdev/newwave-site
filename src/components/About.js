82636c45b07811d2cc7e59aadc6621b55bd8189d
J Holt <jordan.su@protonmail.com>
2020-09-30 14:52:42 -0400

    revised about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,43 +1,26 @@
-import React, {useRef, useEffect } from "react"
+import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Card } from "react-bootstrap";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import aboutStyles from "../styles/about.module.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container, Row, Col } from "react-bootstrap"
+import aboutStyles from "../styles/about.module.css"
 
-gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
 
-  // animation vars
-
-  let aboutTriggerEl = useRef(null);
-  let aboutImageCard = useRef(null);
-
-  useEffect(() => {
-    let introTL = gsap.timeline({
-      scrollTrigger: {
-        trigger: aboutTriggerEl,         
-        start: "top 90%",
-        end: "bottom",
-      },
-    });
-
-    introTL.from(aboutImageCard, {
-      duration: 2,
-      x: '600',
-      opacity: 0,
-      ease: "power3.inout",       
-    })
-  });
 
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "multiple-windmills.jpg" }) {
+      packet1: file(relativePath: { eq: "green-packet.png" }) {
         childImageSharp {
-          fluid(maxWidth: 900) {
+          fluid(maxWidth: 800) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      packet2: file(relativePath: { eq: "purple-packet.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 800) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -46,9 +29,52 @@ const About = () => {
   `)
 
   return (
-    <Container className="mt-5" id="about" ref={el => (aboutTriggerEl = el)}>
-      <Row className={aboutStyles.rowContent}>
-        <Col className="m-4 align-self-center">
+    <Container className="mt-5" id="about" >
+      <Row className="py-5 align-items-center">
+        <Col xs={12} md={8}>
+          <h2 className="pb-5">
+            We provide excellence to the part of the value chain we know best
+            and pick great partners to build the rest.
+          </h2>
+        </Col>
+        <Col xs={{ span: 12, offset: 3 }} md={{ span: 2, offset: 1 }}>
+          <div className={aboutStyles.imgContainer}>
+            <Img fluid={data.packet1.childImageSharp.fluid} />
+          </div>
+        </Col>
+      </Row>
+      <Row className="py-5 align-items-center">
+        <Col xs={{ span: 12, offset: 3 }} md={{ span: 3, offset: 1 }}>
+          <div className={aboutStyles.imgContainer}>
+            <Img fluid={data.packet2.childImageSharp.fluid} />
+          </div>
+        </Col>
+        <Col xs={{ span: 12, order: "first" }} md={{ span: 8, order: "last" }}>
+          <p className="lead">
+            From product ideation, value definition, intellectual property
+            development, partner identification and management, through product
+            development, packaging and launch, we fill the gaps in the value
+            chain, creating world-class solutions for the world’s leading
+            brands.
+          </p>
+        </Col>
+      </Row>
+      <Row className="pt-5">
+        <Col xs={12} md={8}>
+          <p className="lead">
+            We deliver industry-leading household cleaning products including
+            single dose, multi-chamber dishwasher and laundry detergent pacs,
+            OXI powered stain removers, and more.
+          </p>
+        </Col>
+        <Col xs={{ span: 12, offset: 3 }} md={{ span: 2, offset: 1 }}>
+          <div className={aboutStyles.imgContainer}>
+            <Img fluid={data.packet1.childImageSharp.fluid} />
+          </div>
+        </Col>
+      </Row>
+      {/* <Row className={aboutStyles.rowContent}> */}
+      {/* <Col className="m-4 align-self-center">
           <Card>
             <Card.Body>
               <h1 className="display-4 text-center">About Us</h1>
@@ -71,14 +97,14 @@ const About = () => {
             </Card.Body>
           </Card>
         </Col>
-        <Col lg={true} className="m-4" ref={el => (aboutImageCard = el)}>
+        <Col lg={true} className="m-4" >
           <div className={aboutStyles.imgContainer}>
           <Img fluid={data.placeholderImage.childImageSharp.fluid} />
           </div>
-        </Col>
-      </Row>
+        </Col> */}
+      {/* </Row> */}
     </Container>
   )
 }
 
-export default About;
+export default About
