5a877be8475171547d68a743e3f839fc86ba3693
J Holt <jordan.su@protonmail.com>
2020-09-29 20:10:17 -0400

    updated hero, inserted logo and removed bg

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,71 +1,67 @@
 import React, { useRef, useEffect } from "react";
-import { useStaticQuery, graphql, Link } from "gatsby";
+import { Link } from "gatsby";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Button } from "react-bootstrap";
-import BackgroundImage from "gatsby-background-image";
 import gsap  from "gsap";
 
+import logo from "../images/logo-nw.svg"
 import heroStyles from "../styles/hero.module.css"
 
 const Hero = () => {
-
   // hero content vars
-  let heroContainer = useRef(null);
-  let title = useRef(null);
-  let copy = useRef(null);
-  let contactBtn = useRef(null);
+  let heroContainer = useRef(null)
+  let title = useRef(null)
+  let copy = useRef(null)
+  let contactBtn = useRef(null)
 
-  let tl = gsap.timeline();
+  let tl = gsap.timeline()
   // animations
   useEffect(() => {
-    tl.to(heroContainer, {visibility: "visible"})
-      .from(title, {duration: 1.5, ease: "power3.out", y: 150,  opacity: 0, delay: 1})
-      .from(copy, {duration: 1, ease: "power3.out", y: 50, opacity: 0 }, "-=1")
-      .from(contactBtn, {duration: 1, ease: "power3.out", y: 50, opacity: 0 }, "-=1")
-  });
-
-  const data = useStaticQuery(graphql`
-    query {
-      indexImage: file(relativePath: { eq: "hero-windmill.jpg" }) {
-        childImageSharp {
-          fluid(maxWidth: 1800) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
+    tl.to(heroContainer, { visibility: "visible" })
+      .from(title, {
+        duration: 1.5,
+        ease: "power3.out",
+        y: 150,
+        opacity: 0,
+        delay: 1,
+      })
+      .from(copy, { duration: 1, ease: "power3.out", y: 50, opacity: 0 }, "-=1")
+      .from(
+        contactBtn,
+        { duration: 1, ease: "power3.out", y: 50, opacity: 0 },
+        "-=1"
+      )
+  })
 
   return (
-    <BackgroundImage
-      fluid={data.indexImage.childImageSharp.fluid}
-      className={heroStyles.bgWrapper}
-      id="home"
-    >
-      <div className={heroStyles.imgFilter}>
-        <div className={heroStyles.heroContent}>
-          <Container style={{ visibility: "hidden"}}  ref={el => (heroContainer = el)}>
-            <div className={heroStyles.heroContentLine1}>            
-                <h1 className={heroStyles.title} ref={el => (title = el)}>
-                  new<span className={heroStyles.waveTitle}>wave</span>
-                </h1>
-            </div>
-            <div className={heroStyles.heroContentLine2} >
-            <p className="lead text-light" ref={el => (copy = el)}>
+    <Container fluid className={heroStyles.heroWrapper}>
+      <div className={heroStyles.heroContent}>
+        <Container
+          style={{ visibility: "hidden" }}
+          ref={el => (heroContainer = el)}
+        >
+          <div className={heroStyles.heroContentLine1}>
+            <img alt="new wave logo" src={logo} ref={el => (title = el)} />
+          </div>
+          <div className={heroStyles.heroContentLine2}>
+            <p className="lead text-dark" ref={el => (copy = el)}>
               The innovation behind the brands you know
             </p>
-            </div>
-              <div className={heroStyles.heroContentLine3} >
-              <Link to="#contact">
-                <Button className={heroStyles.btnStyles} size="lg" ref={el => (contactBtn = el)}>
-                  Contact
-                </Button>
-              </Link>
-              </div>
-          </Container>
-        </div>
+          </div>
+          <div className={heroStyles.heroContentLine3}>
+            <Link to="#contact">
+              <Button
+                className={heroStyles.btnStyles}
+                size="lg"
+                ref={el => (contactBtn = el)}
+              >
+                Contact
+              </Button>
+            </Link>
+          </div>
+        </Container>
       </div>
-    </BackgroundImage>
+    </Container>
   )
 }
 
